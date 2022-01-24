d51842e2cd351a20e785d474b9ce47dd6de3bced
aghiles saal <aghilesaghiles3@gmail.com>
2022-01-24 19:31:38 +0100

    Updates

---
+++ precache-manifest.7b5917ac4872ebe2803674218fce10df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72882221a6121878cb3db88820ae0972",
+    "url": "/dataviz/index.html"
+  },
+  {
+    "revision": "3b4cd8f1b8eb215391a5",
+    "url": "/dataviz/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "d6f860fc196cec068641",
+    "url": "/dataviz/static/css/main.54d43338.chunk.css"
+  },
+  {
+    "revision": "3b4cd8f1b8eb215391a5",
+    "url": "/dataviz/static/js/2.26ca18fe.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/dataviz/static/js/2.26ca18fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6f860fc196cec068641",
+    "url": "/dataviz/static/js/main.a5605c9e.chunk.js"
+  },
+  {
+    "revision": "2f536c55ba0738b69d4a",
+    "url": "/dataviz/static/js/runtime-main.4015efe6.js"
+  }
+]);
